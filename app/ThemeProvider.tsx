c600d929460fd9d3768f1760df523ee18bd581c5
Alkis Gotovos <alkis.gotovos@gmail.com>
2023-11-30 23:14:22 +0100

    Another small color theme update

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -4,7 +4,13 @@ import { ThemeProvider as NextThemeProvider } from 'next-themes'
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemeProvider attribute="class" enableSystem={false}>
+    <NextThemeProvider
+      attribute='class'
+      defaultTheme='light'
+      enableSystem={false}
+      enableColorScheme={false}
+      disableTransitionOnChange={true}
+    >
       {children}
     </NextThemeProvider>
   )
