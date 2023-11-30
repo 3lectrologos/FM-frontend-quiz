9efa4cee1efce0ca575cfd35e74e1ba6298feb1f
Alkis Gotovos <alkis.gotovos@gmail.com>
2023-11-30 23:09:22 +0100

    Disable using system theme

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider as NextThemeProvider } from 'next-themes'
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemeProvider attribute="class">
+    <NextThemeProvider attribute="class" enableSystem={false}>
       {children}
     </NextThemeProvider>
   )
