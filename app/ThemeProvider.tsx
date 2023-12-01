95e48b930d171c848544c1e596da5ba27f962c98
Alkis Gotovos <alkis.gotovos@gmail.com>
2023-12-01 03:05:50 +0100

    Tiny change

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -6,6 +6,7 @@ export default function ThemeProvider({ children }: { children: React.ReactNode 
   return (
     <NextThemeProvider
       attribute='class'
+      enableSystem={true}
       disableTransitionOnChange={false}
     >
       {children}
