d0218096f7558c67d648933af5d41d62c61a4dc7
Alkis Gotovos <alkis.gotovos@gmail.com>
2023-12-01 05:04:58 +0100

    Fix issue with theme toggle on page refresh

--- app/ThemeToggle.tsx
+++ app/ThemeToggle.tsx
@@ -3,9 +3,15 @@
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
 import { twMerge } from 'tailwind-merge'
+import { useState, useEffect } from 'react'
 
 export default function ThemeToggle() {
+  const [ mounted, setMounted ] = useState(false)
   const { theme, setTheme } = useTheme()
+
+  useEffect(() => setMounted(true), [])
+
+  if (!mounted) return null
 
   return (
     <div className={`flex flex-row items-center gap-x-2 tablet:gap-x-4`}>
@@ -25,7 +31,8 @@ export default function ThemeToggle() {
         <div className={`transition flex flex-row shrink-0 w-8 h-5 bg-purple rounded-full p-1 tablet:w-12 tablet:h-7`} />
         <div className={twMerge(
           `absolute top-1 left-1 w-3 h-3 bg-white rounded-full transition tablet:w-5 tablet:h-5`,
-          `${(theme === 'dark') && 'translate-x-full'}`
+          `${(theme === 'dark') && 'translate-x-full'}`,
+          `${theme}`
         )}
         />
       </label>
